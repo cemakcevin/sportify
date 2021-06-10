e4b5a6b789f2b88ca3b8c8774dd6cb6417f79b25
cemakcevin <akcevin.cem@gmail.com>
2021-06-10 10:43:23 -0400

    created the router setting in App.js and created the LoginPage with the specific route to it

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.scss';
+import {BrowserRouter, Redirect, Switch, Route} from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is an App</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+          <Route path="/login" component={LoginPage}/>
+          <Redirect to="/login"/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import './LoginPage.scss';
+
+function LoginPage () {
+
+
+    return (
+        <div></div>
+    )
+}
+
+export default LoginPage;
