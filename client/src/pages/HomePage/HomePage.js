f5c0c4e3e854d4fc2835a5fd753ed85f65ea1375
cemakcevin <akcevin.cem@gmail.com>
2021-06-16 08:04:14 -0400

    installed the dependencies for server

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,55 +1,69 @@
 import './HomePage.scss';
+import React from 'react';
 
 import Avatar from '../../components/Avatar/Avatar';
 
 import avatarUrl from '../../assets/pictures/profile.jpeg';
 
-function HomePage () {
+class HomePage extends React.Component {
 
-    return(
-        <main className="home">
-            <div className="home__profile profile">
-                <Avatar 
-                    className="profile__avatar" 
-                    avatarUrl={avatarUrl} 
-                />
-                <div className="profile__info">
 
-                </div>
-            </div>
-            <div className="home__interaction interaction">
-                <div className="interaction__favourites">
-                    
-                </div>
-                <div className="interaction__feed-container">
-                    <div className="interaction__feed feed">
+    state = {
+        favouriteTeams:[]
+    }
 
-                    </div>
-                    <div className="interaction__news news">
+    componentDidMount() {
+        
+    }
 
-                    </div>
-                </div>
-            </div>
-            <div className="home__updates updates">
-                <div className="updates__container">
-                    
-                </div>
-                <div className="updates__container">
+    render () {
 
-                </div>
-            </div>
-            <div className="home__friends friends">
-                <div className="friends__wrapper">
-                    <div className="friends__container">
-                    
-                    </div>
-                    <div className="friends__container">
+        return(
+            <main className="home">
+                <div className="home__profile profile">
+                    <Avatar 
+                        className="profile__avatar" 
+                        avatarUrl={avatarUrl} 
+                    />
+                    <div className="profile__info">
     
                     </div>
                 </div>
-            </div>
-        </main>
-    )
+                <div className="home__interaction interaction">
+                    <div className="interaction__favourites">
+                        
+                    </div>
+                    <div className="interaction__feed-container">
+                        <div className="interaction__feed feed">
+    
+                        </div>
+                        <div className="interaction__news news">
+    
+                        </div>
+                    </div>
+                </div>
+                <div className="home__updates updates">
+                    <div className="updates__container">
+                        
+                    </div>
+                    <div className="updates__container">
+    
+                    </div>
+                </div>
+                <div className="home__friends friends">
+                    <div className="friends__wrapper">
+                        <div className="friends__container">
+                        
+                        </div>
+                        <div className="friends__container">
+        
+                        </div>
+                    </div>
+                </div>
+            </main>
+        )
+    }
+    
 }
 
 export default HomePage;
